b3cee252a3b71bdecc33b0c53cb6fabd0d6be81a
sergey <110@mobilon.ru>
2025-03-06 17:43:36 +0700

    fix

---
+++ samples/getJwtToken.js
@@ -0,0 +1,12 @@
+const {AuthClient} = require('../dist');
+const {CLIENT_ID, CLIENT_SECRET} = require('./config');
+
+const proxyUrl = 'https://gigachat-auth-proxy.services.mobilon.ru';
+const authClient = new AuthClient(CLIENT_ID, CLIENT_SECRET, {debug: true, url: proxyUrl});
+
+const start = async () => {
+  const data = await authClient.getToken('GIGACHAT_API_PERS');
+  console.log('data', data);
+}
+
+(start)()
--- src/AuthClient.ts
+++ src/AuthClient.ts
@@ -1,56 +1,58 @@
 import axios from 'axios';
+import { requestLogger, responseLogger } from 'axios-logger';
 import {v4} from 'uuid'
 
 const btoa = (str: string) => {
   return Buffer.from(str).toString('base64');
 }
 
+export interface IOptions {
+  debug: boolean;
+  url: string;
+  logger: any;
+}
+
+const AUTH_URL = 'https://ngw.devices.sberbank.ru:9443';
+
 export class AuthClient{
   axios: any;
-  logStore: any
+  client_id: string;
+  client_secret: string;
 
-  constructor(logUrl: string, url: string, logStore: any){
-    this.logStore = logStore
+  constructor(client_id: string, client_secret: string, options: IOptions){
+    this.client_id = client_id;
+    this.client_secret = client_secret;
+
     this.axios = axios.create({
-      baseURL: url,
+      baseURL: options.url || AUTH_URL,
     })
 
-    this.axios.interceptors.request.use((config: any) => {
-      const pre = '**[AuthClient][Request]** '
-      // const url = ' ' + `${import.meta.env.VITE_LOG_AUTH_URL}` + config.url + ' '
-      const data = config.data ? '\n' + JSON.stringify(config.data, null, ' ') : ""
-      logStore.appendLog(pre + config.method + url + data)
-      return config
-    })
-
-    this.axios.interceptors.response.use(function (response: any) {
-      const pre = '**[AuthClient][Response]** '
-      const url = ' ' + logUrl + response.config.url + ' '
-      const data = response.config.data ? '\n' + response.config.data : ""
-      logStore.appendLog(pre + response.config.method + url + data + ' ' + response.status + ': ' + response.statusText)
-      return response.data;
-      }, function (error: any) {
-        const pre = '**[AuthClient][Error]** '
-        const url = ' ' + logUrl + error.config.url + ' '
-        logStore.appendLog(pre + error.config.method + url + error.config.data + ' ' + error.code + ':' + error.response.data.message)
-        return false
-       }
-    ); 
+    if (options.debug) {
+      const config = {
+        prefixText: 'GigachatAuthClient',
+        status: true,
+        headers: false,
+        params: true,
+      };
+      this.axios.interceptors.request.use((request: any) => {
+        return requestLogger(request, config);
+      });
+      this.axios.interceptors.response.use((response: any) => {
+        return responseLogger(response, config);
+      });
+    } 
   }
 
-  async getToken(client_id: string, client_secret: string, scope: string){
-    this.logStore.isLoading = true
-    const authorization = 'Basic ' + btoa(client_id + ':' + client_secret)
-    const rquid = v4()
+  async getToken(scope: string){
+    const headers = {
+      authorization: 'Basic ' + btoa(this.client_id + ':' + this.client_secret),
+      rquid: v4(),
+      'Accept': 'application/json',
+      'Content-Type': 'application/x-www-form-urlencoded',
+    };
+    const url = '/api/v2/oauth';
 
-    const response = await this.axios
-      .post('',{scope: scope}, {headers: {
-        authorization: authorization,
-        rquid: rquid, 
-        'Accept': 'application/json',
-        'Content-Type': 'application/x-www-form-urlencoded',
-      }})
-      this.logStore.isLoading = false
-    return response
+    const response = await this.axios.post(url, {scope}, {headers});
+    return response.data;
   }
 }
